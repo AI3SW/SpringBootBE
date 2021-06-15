92a6b4f62e252407990efc2100bf5224ee48f950
mayuan1 <76088474+myuan01@users.noreply.github.com>
2021-06-15 10:53:13 +0800

    study of rest service

--- src/main/java/com/example/demo/SpringBootBeApplication.java
+++ src/main/java/com/example/demo/SpringBootBeApplication.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class SpringBootBeApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBeApplication.class, args);
 	}
